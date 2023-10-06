8f7fe219110726161e49c3d9b8562b08932ceed3
Pablo Triandafilide <pablo.triandafilide@patagonian.it>
2023-10-06 17:35:20 -0300

    WIP refactor

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,28 @@
+-- Nvim cmp Autocompletion
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "jose-elias-alvarez/null-ls.nvim",
+    {
+      "williamboman/mason.nvim",
+      opts = {
+        ui = {
+          border = "rounded",
+        },
+      },
+    },
+    "folke/neodev.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  event = "VeryLazy",
+  opts = {
+    mason = {
+      enable = true,
+      auto_install = false,
+    },
+    servers = {
+      phpactor = { enable = true },
+    },
+  },
+}
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,36 @@
+-- LSP Configuration & Plugins
+return { 
+  'neovim/nvim-lspconfig',
+  dependencies  = {
+    -- Automatically install LSPs to stdpath for neovim
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+
+    -- Useful status updates for LSP
+    'j-hui/fidget.nvim',
+  },
+}
+
+require('lspconfig').lua_ls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file('', true),
+        checkThirdParty = false,
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = { enable = false },
+    },
+  },
+}
