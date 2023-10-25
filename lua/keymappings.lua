9b5bc8d9507c4d881398a5904c030a8dc234332c
Pablo Triandafilide <pablotrianda@gmail.com>
2023-10-25 11:58:17 -0300

    Add telescope command to list and create new plugins files

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -72,3 +72,21 @@ vim.keymap.set('v', '<leader>F',
 		require('telescope.builtin').grep_string({ default_text = text })
 	end, { noremap = true, silent = true, desc = '[S]earch [T]ext selected' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>rp', function()
+	require('telescope.builtin').find_files({
+		prompt_title = "Plugins",
+		cwd = "~/.config/nvim/lua/plugins",
+		attach_mappings = function(_, map)
+			local actions = require("telescope.actions")
+			local action_state = require("telescope.actions.state")
+			map("i", "<c-y>", function(prompt_bufnr)
+				local new_plugin = action_state.get_current_line()
+				actions.close(prompt_bufnr)
+				vim.cmd(string.format("edit ~/.config/nvim/lua/plugins/%s.lua", new_plugin))
+			end)
+			return true
+		end
+	})
+end,
+{desc = "[R]ead [P]lugins"}
+)
