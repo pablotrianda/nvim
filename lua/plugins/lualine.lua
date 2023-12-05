e136e376a55cf78479d4a9364932b3aabf880589
Pablo Triandafilide <pablotrianda@gmail.com>
2023-12-05 08:47:32 -0300

    chore: status line new setup

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,16 +1,20 @@
 -- Fancier statusline
 return {
 	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	version = "*",
 	config = function()
 		require('lualine').setup {
 			options = {
-				icons_enabled = false,
+				icons_enabled = true,
 				theme = 'onedark',
 				component_separators = '|',
 				section_separators = '',
+				globalstatus = true,
+			},
+			inactive_winbar = {
+				lualine_c = { "filename" },
 			}
 		}
 	end
-
 }
