9b857c6202c3b820b2940a7da5a5529eac221ac0
Pablo Triandafilide <pablotrianda@gmail.com>
2023-12-18 15:36:35 -0300

    feat: Add node debuger

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,8 +1,46 @@
 --DAP (Debug Adapter Protocol)
+local options = {
+	icons_enabled = true,
+	theme = 'onedark',
+	component_separators = '|',
+	section_separators = '',
+	globalstatus = true,
+}
+local inactive_winbar = {
+	lualine_c = { "filename" },
+}
+
 return {
 	"rcarriga/nvim-dap-ui",
+	event = "VeryLazy",
 	dependencies = {
-		"mfussenegger/nvim-dap"
+		"mfussenegger/nvim-dap",
+		"nvim-lualine/lualine.nvim",
 	},
-	config = true
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+		require("dapui").setup()
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			-- Disable lualine inactive_winbar to avoid cover the debug controls
+			require('lualine').setup {
+				inactive_winbar = {}
+			}
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated["dapui_config"] = function()
+			require('lualine').setup {
+				options = options,
+				inactive_winbar = inactive_winbar
+			}
+			dapui.close()
+		end
+		dap.listeners.before.event_exited["dapui_config"] = function()
+			require('lualine').setup {
+				options = options,
+				inactive_winbar = inactive_winbar
+			}
+			dapui.close()
+		end
+	end
 }
