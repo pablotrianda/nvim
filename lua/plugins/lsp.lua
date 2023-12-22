dd9dd2393b1b52968b19a8451bd141e70b513528
Pablo Triandafilide <pablotrianda@gmail.com>
2023-12-22 00:10:36 -0300

    feat: Add rust tools and config lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -64,5 +64,31 @@ return {
 			}
 		})
 		require('lspconfig').intelephense.setup { on_attach = on_attach, }
+		local opts = {
+			tools = {
+				runnables = {
+					use_telescope = true,
+				},
+				inlay_hints = {
+					auto = true,
+					show_parameter_hints = false,
+					parameter_hints_prefix = "",
+					other_hints_prefix = "",
+				},
+			},
+
+			server = {
+				on_attach = on_attach,
+				settings = {
+					["rust-analyzer"] = {
+						checkOnSave = {
+							command = "clippy",
+						},
+					},
+				},
+			},
+		}
+
+		require("rust-tools").setup(opts)
 	end
 }
