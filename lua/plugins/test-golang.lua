c67eb50f92c84dd243b78c0aea4755544b232b51
Pablo Triandafilide <pablotrianda@gmail.com>
2024-01-27 17:38:01 -0300

    feat: Add plugins

---
+++ lua/plugins/test-golang.lua
@@ -0,0 +1,9 @@
+return{
+	"pablotrianda/test-neovim",
+	dependencies = {
+		"anuvyklack/hydra.nvim",
+		"fatih/vim-go"
+	},
+	config = true,
+	keys = { { "<leader>t", desc = "Open Test menu" } },
+}
