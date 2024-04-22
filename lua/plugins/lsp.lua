b1dea763a5cbddc8a38463524a907320a974e514
Pablo Triandafilide <pablotrianda@gmail.com>
2024-04-22 15:46:23 -0300

    feat(keymap):Add keymap for open diagnostic in line

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -34,6 +34,8 @@ return {
 			nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
 			nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
+			-- Open line diagnostic float
+			nmap('<leader>d',vim.diagnostic.open_float,'[D]iagnostic [Float]')
 			-- Lesser used LSP functionality
 			nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 			nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
